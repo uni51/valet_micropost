29187105878c360574863df44533c847eb3175fd
uni51 <uni51@users.noreply.github.com>
2019-08-31 15:39:07 +0900

    user pages

--- config/app.php
+++ config/app.php
@@ -146,6 +146,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         // 追加
         Collective\Html\HtmlServiceProvider::class,
+        Thomaswelton\LaravelGravatar\LaravelGravatarServiceProvider::class,
     ],
 
     /*
@@ -196,6 +197,7 @@ return [
         // 追加
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Gravatar' => Thomaswelton\LaravelGravatar\Facades\Gravatar::class,
     ],
 
 ];
